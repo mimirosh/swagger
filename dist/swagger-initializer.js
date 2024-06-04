05bdeeb004b85b2a041870658e549d42a517fe42
mimirosh <80176255+mimirosh@users.noreply.github.com>
2024-06-04 23:42:42 +0400

    Update swagger-initializer.js

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://raw.githubusercontent.com/mimirosh/swagger/main/swagger.json",
+    url: "https://mimirosh.github.io/swagger/swagger.json",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
